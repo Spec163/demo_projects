12b5c37696be0efb957f654e23818b20a6171ec0
Artem Gorbatiuk <artem.gorbatiuk@netcracker.com>
2022-03-16 15:27:02 +0400

    feature: added javadoc sample for service layer class

--- demo_eureka_gateway/hero/src/main/java/com/example/hero_service/services/HeroService.java
+++ demo_eureka_gateway/hero/src/main/java/com/example/hero_service/services/HeroService.java
@@ -4,15 +4,28 @@ import com.example.common.clients.enemy.EnemyService;
 import com.example.common.dto.Attributes;
 import com.example.common.dto.EnemyDTO;
 import com.example.common.dto.HeroDTO;
+import feign.FeignException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+/**
+ * HeroService класс сервсисного уровня,
+ * который используется для совершения операций над обработанными запросами
+ *
+ * Пожалуйста, ознакомьтесь с обработчиками веб-запросов
+ * {@link com.example.hero_service.controllers.HeroController}
+ * для понимания возможностей API
+ *
+ * @author Artem
+ *
+ */
 @Slf4j
 @Service
 public class HeroService {
 
+    /** Rest-client for Enemy service */
     private final EnemyService enemyService;
 
     @Autowired
@@ -20,7 +33,14 @@ public class HeroService {
         this.enemyService = enemyService;
     }
 
-    public ResponseEntity<HeroDTO> createHero(final HeroDTO heroDto) {
+    /**
+     * Создаёт героя и отправляет запрос на создание оппонента.
+     *
+     * @param heroDto представление для создания объекта "Hero"
+     * @return http-ответ, в теле которого находится информация о герое
+     * @throws FeignException если не удаётся обратиться к сервису "Enemy"
+     */
+    public ResponseEntity<HeroDTO> createHero(final HeroDTO heroDto)  throws FeignException {
 
         // todo: some operation with entity and db
 
@@ -28,6 +48,8 @@ public class HeroService {
         final EnemyDTO enemyDTO = this.enemyService
             .createEnemy(new EnemyDTO("Opponent_" + heroDto.getName(), Attributes.STRENGTH))
             .getBody();
+
+        // test exception
 
         log.info("EnemyDTO from feign client: {}", enemyDTO);
 
